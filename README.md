1248b290081ac53443478d2835e08b65cdabec4a
Owen Wang <wangfowen@gmail.com>
2018-11-30 22:57:42 -0500

    initial scraper

---
+++ README.md
@@ -0,0 +1,18 @@
+# Setup
+
+Make sure you have Ruby installed.
+
+```
+gem install bundler
+bundle
+```
+
+# Usage
+
+Expected input is a csv of amazon URLs
+
+```
+ruby scraper.rb <INPUT CSV>
+```
+
+Output is a `output.csv` file in the folder you're at
---
+++ scraper.rb
@@ -0,0 +1,70 @@
+require 'open-uri'
+require 'csv'
+require 'nokogiri'
+
+input = CSV.read(ARGV[0])
+output = []
+output << ["Price", "Product Name", "Category", "Subcategory", "ASIN", "URL"]
+
+input.each do |x|
+  url = x[0]
+
+  if !url.nil? && url.include?("amazon")
+    begin
+      docs = Nokogiri::HTML(open(url,
+        "User-Agent" => "Ruby/#{RUBY_VERSION}",
+        "Referer" => "http://www.ruby-lang.org/"
+      ))
+
+      price = nil
+      begin
+        price = docs.css("#priceblock_ourprice").children.first.text.strip
+      rescue Exception => e
+      end
+      if price.nil?
+        puts "price wasn't found for url #{url}"
+      end
+
+      name = nil
+      begin
+        name = docs.css("#productTitle").children.first.text.strip
+      rescue Exception => e
+      end
+      if name.nil?
+        puts "name wasn't found for url #{url}"
+      end
+
+      category = nil
+      begin
+        category =  docs.xpath("//a[@class='a-link-normal a-color-tertiary']").children.first.text.strip
+      rescue Exception => e
+      end
+      if category.nil?
+        puts "category wasn't found for url #{url}"
+      end
+
+      subcategory = nil
+      begin
+        subcategory = docs.xpath("//a[@class='a-link-normal a-color-tertiary']").children.last.text.strip
+      rescue Exception => e
+      end
+      if subcategory.nil?
+        puts "subcategory wasn't found for url #{url}"
+      end
+
+      asin_regex = "(?:[/dp/]|$)([A-Z0-9]{10})"
+      asin = url.match(asin_regex)[1]
+      if asin.nil?
+        puts "asin wasn't found in url #{url}"
+      end
+
+      output << [price, name, category, subcategory, asin, url]
+
+      sleep 1
+    rescue Exception => e
+      puts "couldn't process url #{url}: #{e}"
+    end
+  end
+end
+
+File.write("output.csv", output.map(&:to_csv).join)
